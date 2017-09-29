12ad56f9008c050d991fe457e78fe5d8764d8c92
Jose Escudero <josescuderoh@gmail.com>
2017-09-29 12:04:46 -0500

    Added functionality for password reset, including templates and required views. Some modifications were required for urls, including a new templates folder and direct inclusion of urls in main urls.py

--- src/monitor_briceno/monitor_briceno/urls.py
+++ src/monitor_briceno/monitor_briceno/urls.py
@@ -17,11 +17,22 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^entities/', include('entities.urls')),
     url(r'^projects/', include('projects.urls')),
+    # Urls required for password reset
+    url(r'^password_reset/$', auth_views.password_reset,
+        name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done,
+        name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm,
+        name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete,
+        name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
